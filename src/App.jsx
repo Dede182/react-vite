87d16d2c36e429f7f6782516519afefdfb79efb9
Dede182 <htetshine.htetmkk@gmail.com>
2022-09-30 21:31:18 +0630

    after v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './App.css';
+import { CgSpinnerTwo } from 'react-icons/cg'
 const App = () => {
   return (
-    <div className='flex justify-center text-red-900 text-2xl'>
-      app
+    <div className='flex justify-center h-[40vh] items-center  text-red-900 text-2xl'>
+      <button type="button" class="flex flex-row items-center" >
+    
+          <CgSpinnerTwo class="animate-spin h-5 w-5 mr-3 ..."  />
+      
+        Processing...
+      </button>
     </div>
   )
 }
