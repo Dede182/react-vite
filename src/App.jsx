5ba4e82058fe54acfdc5ef0221cdd74d3aa55b10
Dede182 <htetshine.htetmkk@gmail.com>
2022-09-30 21:37:13 +0630

    after react-icons:

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.css';
-import { CgSpinnerTwo } from 'react-icons/cg'
+import { GiSpinningBlades } from 'react-icons/gi'
 const App = () => {
   return (
-    <div className='flex justify-center h-[40vh] items-center  text-red-900 text-2xl'>
-      <button type="button" class="flex flex-row items-center" >
+    <div className='flex justify-center h-[40vh] items-center  text-5xl'>
+      <button type="button" class="flex flex-row text-[#4D4D4D] items-center" >
     
-          <CgSpinnerTwo class="animate-spin h-5 w-5 mr-3 ..."  />
+          <GiSpinningBlades class="animate-spin text-[#40C057] h-20 w-20 mr-3 ..."  />
       
         Processing...
       </button>
