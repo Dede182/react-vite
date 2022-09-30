d75d43bd4a96ccdffeeb3aab11e39657d9eb7ae2
Dede182 <htetshine.htetmkk@gmail.com>
2022-09-30 19:03:46 +0630

    some

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css';
+const App = () => {
+  return (
+    <div className='flex justify-center text-red-900 text-2xl'>
+      app
+    </div>
+  )
+}
+
+export default App
